3012934fb73e902efedb1d60be1fb3eba1087f8d
waddah-jd <waddah.ja@gmail.com>
2020-03-29 11:39:59 +0300

    Updates

---
+++ precache-manifest.6fcbdf2edaac8b4263f34e086f46e0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55946ad6c11aa33aa5c702fa34c9ef90",
+    "url": "/crypto-converter/index.html"
+  },
+  {
+    "revision": "58b6b1be6ea04c9f19b8",
+    "url": "/crypto-converter/static/css/main.1483c20d.chunk.css"
+  },
+  {
+    "revision": "6a4359225ff92e366eb0",
+    "url": "/crypto-converter/static/js/2.5ffa8a03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crypto-converter/static/js/2.5ffa8a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b6b1be6ea04c9f19b8",
+    "url": "/crypto-converter/static/js/main.26831979.chunk.js"
+  },
+  {
+    "revision": "1400f08f59b8b3f856ad",
+    "url": "/crypto-converter/static/js/runtime-main.68d89627.js"
+  }
+]);
